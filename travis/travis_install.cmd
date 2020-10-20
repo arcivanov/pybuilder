b210f37c83b5d3e538c1b908c0ff20ff3cb8fc3e
Arcadiy Ivanov <arcadiy@ivanov.biz>
2020-10-20 17:45:36 -0400

    Trying to fix Windows Python failures

--- travis/travis_install.cmd
+++ travis/travis_install.cmd
@@ -1,18 +1,23 @@
-choco upgrade chocolatey
+where python
+python --version
+
 choco config set --name="'webRequestTimeoutSeconds'" --value="'3600'"
 choco config set --name="'commandExecutionTimeoutSeconds'" --value="'14400'"
+choco upgrade chocolatey
+choco list --localonly
 
 choco install -y %PYTHON_PACKAGE% --version=%PYTHON_VERSION% --timeout=14400
 
-set PATH=%PYTHON_BIN%;%PYTHON_BIN%\Scripts;%PATH%
+set PATH=%PYTHON_BIN%\Scripts;%PYTHON_BIN%;%PATH%
 
 pip install virtualenv
-python -m virtualenv \venv
+python -m virtualenv --upgrade-embed-wheels
+python -m virtualenv --copies \venv
 
-set PATH=%VENV_DIR%;%VENV_DIR%\Scripts;%PATH%
+set PATH=%VENV_DIR%;%PATH%
 
 where python
 python --version
 where pip
-pip install -U pip setuptools
 pip --version
+C:\venv\scripts\python -c "import sys; import os.path; print(os.path.realpath(sys.executable), sys.prefix, sys.exec_prefix, sys.base_prefix, sys.base_exec_prefix)"
